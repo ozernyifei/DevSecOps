b1c05a925b31e9139daecab485113c1516886105
Toadles <ozernyifei@outlook.com>
2023-04-09 19:13:35 +0300

    saf

--- Math.cpp
+++ Math.cpp
@@ -1,6 +1,17 @@
 #include "Math.h"
+#include <iostream>
 
 double Math::add(double num1, double num2) { return num1 + num2; }
-double Math::subtract(double num1, double num2) { return num1 + num2; }
+double Math::subtract(double num1, double num2) { return num1 - num2; }
 double Math::multiply(double num1, double num2) { return num1 * num2; }
-double Math::divide(double num1, double num2) { return num1 / num2; }
+double Math::divide(double num1, double num2) 
+{ if (num2 != 0)
+{
+    return num1 / num2;
+}
+else
+{
+    std::cout << "Don't divide by zero!";
+    return 0;
+}
+ }
